4f01d069c83060b281dc04628dffecb823d2fc32
noelxyz <koutan1113.2001@gmail.com>
2018-09-15 21:07:47 +0900

    Update login.py

--- linepyopt/login.py
+++ linepyopt/login.py
@@ -11,7 +11,7 @@ def token_login(token, poll=False):
 	else:
 		endpoint = Config.normal_endpoint
 	client = TalkService.Client(LineWrapper.get_protocol(endpoint, headers))
-	client.authToken = res.authToken
+	client.authToken = authToken
 	return client
 
 
